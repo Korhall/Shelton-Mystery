7a6e33d456b0bd97235187224d034913ca13b91e
Korhall <korhall@yandex.ru>
2016-02-25 14:23:54 +0300

    Основной класс

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import javax.swing.*;
+import java.awt.ActiveEvent.*;
+import java.awt.Event.*;
+import java.awt.*;
+
+import com.company.GUIClass;
+
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+
+       GUIClass guiClass = new GUIClass();
+       // System.out.print("апрекр ке");
+	// write your code here
+    }
+}
