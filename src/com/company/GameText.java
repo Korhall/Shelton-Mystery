62cfabac1cbd85e8c2ee5314e7f8cf0ba5d7b17d
Anton Smirnov <korhall@yandex.ru>
2016-02-25 14:23:12 +0300

    Текст книги.

---
+++ src/com/company/GameText.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by srv3 on 28.01.2016.
+ */
+public class GameText
+{
+    String[] gameText = {"pokj4egoi jrtg  geriog oiregeruher gerugh ihg regregh ohgurweeugh reugwgh erugh uwg ", "", "" };
+}
