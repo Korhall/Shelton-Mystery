e77c4064bf708975a314fb652a6db3b980d27377
Anton Smirnov <korhall@yandex.ru>
2016-04-13 12:13:20 +0300

    Исправлено отображение графических элементов. Добавлено текстовое поле в managePanel для ввода номера параграфа.

--- src/com/korhall/objects/GUIClass.java
+++ src/com/korhall/objects/GUIClass.java
@@ -2,6 +2,7 @@ package com.korhall.objects;
 
 import javax.swing.*;
 import java.awt.*;
+
 /**
  * Created by srv3 on 27.01.2016.
  */
@@ -14,46 +15,56 @@ public class GUIClass
     JPanel managePanel;
     JTextArea mainText;
     JButton nextButton;
+    JTextField textFieldNextPoint;
 
 
     public GUIClass ()
     {
-       mainFrame = new JFrame("Тайна капитана Шелтона");
+        mainFrame = new JFrame("Тайна капитана Шелтона");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+//        блок инициализации графических компонентов
         textPanel = new JPanel();
         GUIPanel = new JPanel();
         charPanel = new JPanel();
         managePanel = new JPanel();
         nextButton = new JButton("Далее");
-
         mainText = new JTextArea();
+        textFieldNextPoint = new JTextField();
 
 
 
+// блок задания размеров элементов
 
-        mainFrame.getContentPane().add(BorderLayout.CENTER, textPanel);
-        mainFrame.getContentPane().add(BorderLayout.NORTH, GUIPanel);
-        mainFrame.getContentPane().add(BorderLayout.EAST, charPanel);
-        mainFrame.getContentPane().add(BorderLayout.SOUTH, textPanel);
+        mainFrame.setSize(1024, 1280);
+        textFieldNextPoint.setSize(100, 20); // не работает надо выяснить, почему
+
+
+        // добавление элементов к панелям
+        managePanel.add(textFieldNextPoint, FlowLayout.LEFT);
+      
+
 
         managePanel.add(nextButton);
         textPanel.add(mainText);
 
 
 
-
-
-
-
-        mainFrame.add(textPanel);
+       mainFrame.add(textPanel);
        mainFrame.add(GUIPanel);
        mainFrame.add(charPanel);
        mainFrame.add(managePanel);
 
+        //      блок настройки раскладки
+
+        mainFrame.getContentPane().add(BorderLayout.CENTER, textPanel);
+        mainFrame.getContentPane().add(BorderLayout.NORTH, GUIPanel);
+        mainFrame.getContentPane().add(BorderLayout.EAST, charPanel);
+        mainFrame.getContentPane().add(BorderLayout.SOUTH, managePanel);
 
 
-        mainFrame.setSize(1024, 1280);
+
         mainFrame.setVisible(true);
 
 
